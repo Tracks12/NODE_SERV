16cf7258f44f00ed5540a39104c52bc6a5789030
Anarchy <anon.dark97@gmail.com>
2018-05-26 13:57:01 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var conf = require('./conf.json'),
-    http = require('http'),
-    url = require('url'),
-    fs = require('fs');
+		http = require('http'),
+		url = require('url'),
+		fs = require('fs');
 
 function onRequest(request, response) {
 	var pathname = url.parse(request.url).pathname,
-	    extension = pathname.split('.').pop(),
-	    today = new Date(),
-	    result = '['+today.getHours()+':'+today.getMinutes()+':'+today.getSeconds()+'] ';
+			extension = pathname.split('.').pop(),
+			today = new Date(),
+			result = '['+today.getHours()+':'+today.getMinutes()+':'+today.getSeconds()+'] ';
 
 	if(pathname == '/') { pathname = conf.http.index; extension = 'html'; }
 
@@ -25,8 +25,20 @@ function onRequest(request, response) {
 }
 
 switch(process.argv[2]) {
-	case '-start':
-	default: http.createServer(onRequest).listen(conf.http.port);
+	case '-ls': var files = new Array(), i = new Array(0, 0);
+		fs.readdirSync('.').forEach(file => { files[i[0]] = file; i[0]++; });
+		fs.readdirSync(files[3]).forEach(file => { files[i[1]] = file; i[1]++; });
+		console.log(files);
+		break;
+	case '-start': http.createServer(onRequest).listen(conf.http.port);
 		console.log('Node JS Server is starting...\n');
+		break;
+	case '-h':
+	case '--help':
+	default: var helper = 'server.js  -start\t: Démarre un serveur local sur le port '+conf.http.port+'\n';
+		helper += '\t   -ls\t\t: Liste les fichiers présents dans le répertoire "'+conf.http.www+'"\n';
+		helper += '\t   -h\t\t: Affiche les arguments de commandes\n\n';
+		helper += '\t   --help\t: Affiche les arguments de commandes\n\n';
+		console.log(helper);
 		break;
 }
