9c9c9d7d850a153bd356003ebf19b2d9170764dc
Anarchy <anon.dark97@gmail.com>
2018-05-27 02:09:17 +0200

    Add files via upload

--- www/js/script.js
+++ www/js/script.js
@@ -1,14 +1,5 @@
 // script.js
 
-function check() {
-	switch(window.location.pathname.split('.')[0]) {
-		case '/about': document.getElementsByClassName('nav')[2].style.backgroundColor = '#444444'; break;
-		case '/code': document.getElementsByClassName('nav')[1].style.backgroundColor = '#444444'; break;
-		case '/index':
-		default: document.getElementsByClassName('nav')[0].style.backgroundColor = '#444444'; break;
-	}
-}
-
 function startTime(sep) {
 	var today = new Date(), delay = 500;
 	var h = today.getHours(), m = today.getMinutes();
@@ -21,6 +12,30 @@ function startTime(sep) {
 	} else { var s = ''; delay = 1000; if(sep === ":") { sep = " "; } else { sep = ":"; }}
 	document.getElementById('time').innerHTML = h+sep+m+s;
 	t = setTimeout(function() { startTime(sep); }, delay);
+}
+
+function check() {
+	switch(window.location.pathname.split('.')[0]) {
+		case '/about': document.getElementsByClassName('nav')[3].style.backgroundColor = '#444444'; break;
+		case '/code': document.getElementsByClassName('nav')[2].style.backgroundColor = '#444444'; break;
+		case '/tree': document.getElementsByClassName('nav')[1].style.backgroundColor = '#444444'; break;
+		case '/index':
+		default: document.getElementsByClassName('nav')[0].style.backgroundColor = '#444444'; break;
+	}
+}
+
+function editor() {
+	var pre = new Array(
+		document.getElementsByTagName('pre')[0],
+		document.getElementsByTagName('pre')[1]
+	), s;
+	
+	for(var i = 0; i < 2; i++) {
+		for(var j = 0; j < pre[i].getElementsByTagName('li').length; j++) {
+			if(j < 10) { s = ' '+j; } else { s = j; }
+			pre[i].getElementsByTagName('li')[j].innerHTML = s+' | '+pre[i].getElementsByTagName('li')[j].innerHTML
+		}
+	}
 }
 
 // END
